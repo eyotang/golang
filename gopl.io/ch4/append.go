a43d759672307cb05f940b0aa88e596636fe551a
Ben Tang <khan.tang.h@gmail.com>
2017-06-30 09:47:25 +0800

    add append

---
+++ gopl.io/ch4/append.go
@@ -0,0 +1,45 @@
+package main
+
+import ("fmt")
+
+func appendInt(x []int, y ...int) []int {
+	var z []int
+	zlen := len(x) + 1
+	if zlen <= cap(x) {
+		z = x[:zlen]
+	} else {
+		zcap := zlen
+		if zcap < 2*len(x) {
+			zcap = 2*len(x)
+		}
+		z = make([]int, zlen, zcap)
+		copy(z, x)
+	}
+	copy(z[len(x):], y)
+	return z
+}
+
+
+func main() {
+	a := [...]int{0, 1, 2, 5}
+	b := appendInt(a[:], 8)
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Printf("len(a): %d, cap(a): %d, len(b): %d, cap(b): %d\n", len(a), cap(a), len(b), cap(b))
+
+	var x, y []int
+	for i := 0; i < 10; i++ {
+		y = appendInt(x, i)
+		fmt.Printf("%d cap=%d\t%v\n", i, cap(y), y)
+		x = y
+	}
+
+	var runes []int
+	for i := 0; i < 10; i++ {
+		runes = append(runes, i)
+		fmt.Printf("%d cap=%d\t%v\n", i, cap(runes), runes)
+	}
+
+	b = appendInt(a[:], 3, 4, 9)
+	fmt.Println(b)
+}
--- gopl.io/ch4/rev.go
+++ gopl.io/ch4/rev.go
@@ -8,8 +8,30 @@ func reverse(s []int) {
 	}
 }
 
+func equal(x, y []string) bool {
+	if len(x) != len(y) {
+		return false
+	}
+
+	for i := range x {
+		if x[i] != y[i] {
+			return false
+		}
+	}
+	return true
+}
+
 func main() {
 	a := [...]int{0, 1, 2, 3, 4, 5}
 	reverse(a[:])
 	fmt.Println(a)
+
+	b := [...]string{"abc", "ef"}
+	fmt.Println(equal(b[:], b[:]))
+
+	var runes []rune
+	for _, r := range "Hello, world" {
+		runes = append(runes, r)
+	}
+	fmt.Printf("%q\n", runes)
 }
